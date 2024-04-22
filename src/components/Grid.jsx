c1fc6b6c7dabd554bc2d86768ec322938877c1b2
Eduasso10 <eduardolomasrecena@gmail.com>
2024-04-22 18:45:17 +0200

    Mapa visionado

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,50 +1,52 @@
-import React, { useState, useEffect } from 'react'; // Importa useState y useEffect desde 'react'
-import mqtt from 'mqtt'; // Asegúrate de importar el paquete mqtt
+import React, { useState, useEffect } from 'react';
+import mqtt from 'mqtt';
 import Square from './Square';
 
 function Grid() {
-  const [messageReceived, setMessageReceived] = useState(false);
+  const [mapData, setMapData] = useState(null);
 
   useEffect(() => {
-    const client = mqtt.connect('ws://192.168.48.245:8083'); // Conecta usando WebSocket
+    const client = mqtt.connect('ws://192.168.48.245:8083');
 
     client.on('connect', () => {
       console.log('Conectado al servidor MQTT');
-      client.subscribe('map'); // Suscríbete al tema 'map'
+      client.subscribe('map');
     });
 
     client.on('message', (topic, message) => {
-      if (!messageReceived) {
-        const color = message.toString();
-        console.log("Mensaje recibido: ", message.toString());
-        setMessageReceived(true);
-        client.unsubscribe('map'); // Desuscribirse del tema para no recibir más mensajes
-        client.end(); // Terminar conexión
+      if (!mapData) {
+        const mapString = message.toString();
+        console.log("Mensaje recibido:", mapString);
+        setMapData(mapString);
+        client.unsubscribe('map');
+        client.end();
       }
     });
 
-    // Manejar errores de conexión
     client.on('error', (error) => {
       console.error('Error de conexión:', error);
     });
 
-    // Devolver una función de limpieza para desconectar el cliente MQTT cuando se desmonte el componente
     return () => {
       client.end();
     };
-  }, []); // Se ejecuta solo una vez al montar el componente
+  }, []);
 
-  // Crear una matriz de 7x5
   const rows = 7;
   const cols = 5;
   const grid = [];
 
-  for (let i = 0; i < rows; i++) {
-    const row = [];
-    for (let j = 0; j < cols; j++) {
-      row.push(<Square key={`${i}-${j}`} />);
+  if (mapData) {
+    let index = 0;
+    for (let i = 0; i < rows; i++) {
+      const row = [];
+      for (let j = 0; j < cols; j++) {
+        const id = mapData.substring(index, index + 2);
+        row.push(<Square key={`${i}-${j}`} id={id} />);
+        index += 2;
+      }
+      grid.push(<div key={i} style={{ display: 'flex' }}>{row}</div>);
     }
-    grid.push(<div key={i} style={{ display: 'flex' }}>{row}</div>);
   }
 
   return <div>{grid}</div>;
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,6 +1,15 @@
-// Componente para representar un cuadrado individual
-const Square = () => {
-    return <div style={{ width: '75px', height: '75px', backgroundColor: 'blue', margin: '2px' }}></div>;
+import React from 'react';
+
+const Square = ({ id }) => {
+  // Construye la ruta de la imagen utilizando el prop 'id'
+  const imagePath = `/imagenes/${id}.png`;
+
+  return (
+    <div style={{ width: '75px', height: '75px', margin: '2px' }}>
+      {/* Utiliza la ruta de la imagen como el valor del atributo 'src' */}
+      <img src={imagePath} alt={`Square ${id}`} style={{ width: '100%', height: '100%' }} />
+    </div>
+  );
 };
 
 export default Square;
